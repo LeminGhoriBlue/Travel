679893c6b2732cce27cea2a743c6b90a10645ac2
LeminGhoriBlue <leminghori@gmail.com>
2023-08-17 15:59:26 +0530

    add

--- frontend/src/Comman/Form/SignIn.jsx
+++ frontend/src/Comman/Form/SignIn.jsx
@@ -23,7 +23,7 @@ function SignInForm({ setUserValied }) {
       return false;
     }
     await axios
-      .post(`https://free-portfoilo-backend.onrender.com/login`, {
+      .post(`https://travel-backend-lxsg.onrender.com/login`, {
         name: state.name,
         password: state.password,
       })
